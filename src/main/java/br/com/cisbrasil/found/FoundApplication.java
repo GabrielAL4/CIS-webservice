34cac14c13da14ecda52c9aabbef6fdebc0638b8
Gabriel Albuquerque <gabriel.albuquerque2001@gmail.com>
2023-06-27 13:15:54 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/cisbrasil/found/FoundApplication.java
@@ -0,0 +1,13 @@
+package br.com.cisbrasil.found;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundApplication.class, args);
+	}
+
+}
