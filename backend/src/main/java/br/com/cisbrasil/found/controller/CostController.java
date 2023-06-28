af75fb644c9828c132b22a5c76ab8ff1d53196df
Gabriel Albuquerque <gabriel.albuquerque2001@gmail.com>
2023-06-28 13:54:12 -0300

    Commit com controller de custo criado

--- backend/src/main/java/br/com/cisbrasil/found/controller/CostController.java
+++ backend/src/main/java/br/com/cisbrasil/found/controller/CostController.java
@@ -1,12 +1,34 @@
 package br.com.cisbrasil.found.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.cisbrasil.found.model.Cost;
+import br.com.cisbrasil.found.service.CostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cost")
 @CrossOrigin("https:/localhost:3000/")
 public class CostController {
-
+    @Autowired
+    CostService service;
+    @GetMapping
+    List<Cost> findAll(){
+        return service.findAll();
+    }
+    Cost save(@RequestBody Cost cost){
+        return service.save(cost);
+    }
+    @DeleteMapping("{costId}")
+    @ResponseStatus
+    void delete(@PathVariable("costId") int costId){
+        service.delete(costId);
+    }
+    @PatchMapping("{costId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    Cost update(@RequestBody Cost costToUpdate, @PathVariable("costId") int costId){
+        return service.update(costToUpdate, costId);
+    }
 }
